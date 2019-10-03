eb2f9e65f5bb5ac5953b8d55d3e6cfbaf847c9e7
beyzak12 <beyzakurt1998@hotmail.com>
2019-10-03 15:19:22 +0300

    lab3

---
+++ ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.listviewexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.time.Instant;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button arrayAdapterButton;
+    Button customAdapterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arrayAdapterButton = findViewById(R.id.array_adapter);
+        customAdapterButton = findViewById(R.id.custom_adapter);
+
+        AdapterButtonListener listener = new AdapterButtonListener();
+        //View.OnClickListener listener = new AdapterButtonListener();
+
+        arrayAdapterButton.setOnClickListener(listener);
+
+
+        View.OnClickListener listener2 = new View.OnClickListener(){
+            //anonymous class
+            @Override
+            public void onClick(View v) {
+                Log.d("AdapterButtonListener","Clicked");
+                Intent intent = new Intent(MainActivity.this,ArrayAdapterActivity.class);
+                startActivity(intent);
+            }
+        };
+
+
+        customAdapterButton.setOnClickListener(listener);
+
+
+    }
+
+    private class AdapterButtonListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Log.d("AdapterButtonListener","Clicked");
+            Intent intent = new Intent(MainActivity.this,ArrayAdapterActivity.class);
+            startActivity(intent);
+        }
+    }
+}
